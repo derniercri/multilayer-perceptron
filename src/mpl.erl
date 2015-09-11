81933b6d1d77f7c1053fae323738be9bea51b853
altor <arthur.dazemar@free.fr>
2015-09-11 17:40:47 +0000

    fix bug

--- src/mpl.erl
+++ src/mpl.erl
@@ -2,12 +2,13 @@
 -behaviour(gen_server).
 
 
--export([start_link/0]).
+-export([start_link/3, compute/1, train/2]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
-start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Layers_values, Nb_inputs, Nb_layer) -> 
+gen_server:start_link(?MODULE, {Layers_values, Nb_inputs, Nb_layer}, []).
 
 %% ----------
 %% Interface
@@ -33,11 +34,11 @@ init({Layers_values, Nb_inputs, Nb_layer}) ->
 
     %% initialisation de l'état du serveur
     [{Nb_outputs, _, _} | _] = Layers_values,
-    State = init_state(Network_value, Nb_outputs, Trainer)
+    State = init_state(Network_value, Nb_outputs, Trainer),
 
     %% Initialisation du processus de sortie
-    Output = spawn(fun() -> neuron:output_serv(?SERVER) end),
-    neuron:connect_output(Output, Output_list)
+    Output = spawn(fun() -> neuron:output_serv(?MODULE) end),
+    neuron:connect_output(Output, Output_list),
 
     {ok, State}.
 
@@ -46,35 +47,36 @@ handle_call({compute, Values_list}, From, State) ->
     case get_network_state(State) of
 	%% si le réseau est disponible, on lance le calcul
 	ready -> 
-	    Input_list = get_input_list(State),
+	    Inputs_list = get_input_list(State),
 	    F = fun ({Input, Val}) -> Input ! {input, Val} end,
 	    ok = lists:foreach(F, lists:zip(Inputs_list, Values_list)),
-	    New_state = set_network_state(is_computing, State) ->
-	    {reply, launched, New_state};
+	    New_state = set_network_state(is_computing, State),
+	    New_state2 = add_from(New_state, From),
+	    {reply, launched, New_state2};
 	%% sinon on renvoi une réponse négative
 	Other -> {reply, Other, State}
-    end.
+    end;
 
-handle_call({train, Training_values, Training_constant}, State) -> 
+handle_call({train, Training_values, Training_constant}, From, State) -> 
     case get_network_state(State) of
 	%% si le réseau est disponible, on lance l'entrainement
 	ready -> 
 	    {Trainer, Input_list} = get_training_value(State),
-	    trainer:launch(Trainer, Input_list, Training_values, Training_constant)
+	    trainer:launch(Trainer, Input_list, Training_values, Training_constant);
 	%% sinon on renvoi une réponse négative
 	Other -> {reply, Other, State}
     end.
 
 
-handle_cast({output, {Rank, Value}}, State) -> 
+handle_cast({output, Output}, State) -> 
     New_state = add_output(Output, State),
-    if 
-	is_output_ok(New_state) ->
+    case is_output_ok(New_state) of
+	true ->
 	    {Client, Ordoned_list} = get_answer_info(State),
 	    gen_server:reply(Client, {compute_done, Ordoned_list}),
 	    {noreply, reset_state(New_state)};
 	
-	true ->
+	_ ->
 	    {noreply, New_state}
     end.
 	    
@@ -82,26 +84,26 @@ handle_cast({output, {Rank, Value}}, State) ->
 
 handle_info(_Info, State) -> {noreply, State}.
 terminate(_Reason, _State) -> ok.
-code_change(_OldVsn, State, Extra) -> {ok, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 %% -----------------------------------
 %% Fonction de manipulation de l'état
 %% -----------------------------------
 
 init_state(Network_value, Nb_outputs, Trainer) ->
-    State = {Network_value, Trainer, ready, [], {0, Nb_outputs}}.
+    {Network_value, Trainer, {ready, null}, [], {0, Nb_outputs}}.
 
 reset_state(State) ->
     {Network_value, Trainer, _, _, {_, Nb_outputs}} = State,
-    {Network_value, Trainer, ready, [], {0, Nb_outputs}}.
+    {Network_value, Trainer, {ready, null}, [], {0, Nb_outputs}}.
 
 add_output(Output, State) ->
     {Network_value, Trainer, Network_State, Output_list, {Outputs_computed, Nb_outputs}} = State,
     New_output_list = [Output | Output_list],
-    {Network_value, Trainer, Network_State, New_Output_list, {Outputs_computed + 1, Nb_outputs}}
+    {Network_value, Trainer, Network_State, New_output_list, {Outputs_computed + 1, Nb_outputs}}.
 
 get_input_list(State) ->
-    {Network_value, _, _, _},
+    {Network_value, _, _, _} = State,
     {_, Input_list, _, _} = Network_value,
     Input_list.
 
@@ -121,13 +123,15 @@ get_network_state(State) ->
     Network_State.
 
 set_network_state(New_network_state, State) ->
-    {Network_value, Trainer, {Network_State, From}, Output_list, Output_info} = State,
-    {Network_value, Trainer, {New_network_State, From}, Output_list, Output_info}.
+    {Network_value, Trainer, {_, From}, Output_list, Output_info} = State,
+    {Network_value, Trainer, {New_network_state, From}, Output_list, Output_info}.
 
 get_training_value(State) ->
     {Network_value, Trainer, _, _, _} = State,
     {_, Input_list, _, _} = Network_value,
     {Trainer, Input_list}.
     
-    
+add_from(State, From) ->
+    {Network_value, Trainer, {Network_state,_}, Output_list, Output_info} = State,
+    {Network_value, Trainer, {Network_state, From}, Output_list, Output_info}.
     
